5ee592f3db89dcf280cf48e9f398e67216adc241
tranquangnhan <tranquangnhan1606@gmail.com>
2021-02-02 22:14:34 +0700

    header

--- index.php
+++ index.php
@@ -1,50 +1,67 @@
 <?php
+    /** Config Return Header */
+    header("Access-Control-Allow-Origin: *");
+    header('Content-type:application/json;charset=utf-8');
+    
     require_once "models/database.php";
     require_once "models/class.php";
+
     if(isset($_GET['act'])){
         switch ($_GET['act']) {
             case 'lopadd':
-                $viewFile = "views/classadd.php";    
+                   
                 if(isset($_POST['add']) && ($_POST['add'])) {
                     $class = $_POST['class'];
                     if(insertClass($class)){
                         header("location: ?act=lopindex");
                     }
                 }
+
+                $viewFile = "views/classadd.php";
                 require_once "views/layout.php";
                 break;
             case 'lopindex':
-                $viewFile = "views/classindex.php";     
+               
                 $getAllClass =  getAllClass();
+
+                $viewFile = "views/classindex.php";     
                 require_once "views/layout.php";
                 break;
             case 'lopdel':
-                $viewFile = "views/classindex.php";     
+                
                 if(isset($_GET['id'])){
                     if( delClass($_GET['id'])){
                         header("location: ?act=lopindex");
                     }
                 }
+
+                $viewFile = "views/classindex.php";   
                 require_once "views/layout.php";
                 break;
 
             case 'lessonadd':
-                $viewFile = "views/lessonadd.php";    
+                
+                    
                 if(isset($_POST['add']) && ($_POST['add'])) {
                     $idkhoayt = $_POST['idkhoayt'];
                     $idclass = $_POST['idclass'];
                     $idmon = $_POST['idmon'];
                     $allFile = $_FILES['img'];   
                     $img = checkUpLoadMany($allFile);
+
                     if( insertLesson($idkhoayt,$img,$idclass,$idmon)){
                         header("location: ?act=lessonindex");
                     }
                 }
+
+                $viewFile = "views/lessonadd.php";
                 require_once "views/layout.php";
                 break;
             case 'lessonindex':
-                $viewFile = "views/lessonindex.php";     
+    
                 $getAllLesson =  getAllLesson();
+                
+                $viewFile = "views/lessonindex.php"; 
                 require_once "views/layout.php";
                 break;
             case 'lessondel':
@@ -57,6 +74,7 @@
                 require_once "views/layout.php";
                 break;
             case 'listclasses':
+                
                 $array = array();
                
                 $array['data'] =  getAllClass();
